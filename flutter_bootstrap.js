de5bc5289e399f873f0b754f0c5c603e63f829a8
gh93st <62462574+Muhammad-Radwan@users.noreply.github.com>
2026-03-11 15:17:50 +0200

    some minor bugs, delete functionality

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2329710034"
+    serviceWorkerVersion: "1109109537"
   }
 });
